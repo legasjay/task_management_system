4bc393b9a97e54198e78d17387a2807e7a2f1ad8
01egas <38017925+01egas@users.noreply.github.com>
2024-12-26 23:51:12 +0300

    Добавлено развертывание в kubernetes (докерфайл, конфиги для kubernetes). В readme добавлены инструкции. Файл application.yaml изменен для запуска в kubernetes, но можно раскомментировать для запуска в docker-compose. Удалены файлы, которые не использовались (файлы миграции). Из build.gradle удалена версия для упрощения имени приложения.

--- NotUse/V1_0_1__add_foreign_keys.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tasks ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES users(id);
-ALTER TABLE tasks ADD CONSTRAINT fk_assignee FOREIGN KEY (assignee_id) REFERENCES users(id);
-ALTER TABLE comments ADD CONSTRAINT fk_task FOREIGN KEY (task_id) REFERENCES tasks(id);
-ALTER TABLE comments ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id);
